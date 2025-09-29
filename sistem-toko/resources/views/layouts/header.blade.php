7d10701d4b707402e841a19225aa2e3907b52ff2
aapingg <aprilliaokta88@gmail.com>
2025-09-29 14:14:17 +0700

    uts: soal 4

---
+++ sistem-toko/resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<header style="padding:10px; border-bottom:1px solid #ccc;">
+    <nav>
+        <a href="{{ route('uts.index') }}">UTS Home</a> |
+        <a href="{{ route('uts.web') }}">UTS Pemrograman Web</a> |
+        <a href="{{ route('uts.database') }}">UTS Database</a>
+    </nav>
+</header>
